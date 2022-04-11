bd5b5a67eacd8e6c65b549a4c3d62f4a71a18355
nakhuong269 <75614193+nakhuong269@users.noreply.github.com>
2022-04-11 22:55:08 +0700

    GUI Login/Register/ForgetPass

--- QLHS/MainWindow.xaml.cs
+++ QLHS/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using QLHS.Views;
 namespace QLHS
 {
     /// <summary>
@@ -23,6 +23,7 @@ namespace QLHS
         public MainWindow()
         {
             InitializeComponent();
+            Main.Content = new Login();
         }
     }
 }
